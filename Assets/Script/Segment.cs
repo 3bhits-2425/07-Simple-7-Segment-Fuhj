8047a05fcc9b1f482b487382ddd1bfdb09190503
Jonah Fuhrmeister <130558732+Jonus153@users.noreply.github.com>
2025-03-05 15:45:17 +0100

    new

--- Assets/Script/Segment.cs
+++ Assets/Script/Segment.cs
@@ -1,119 +1,73 @@
 using UnityEngine;
+using System.Collections;
 
 public class RotateSegment : MonoBehaviour
 {
     public Transform[] segments; // Array von Segmenten
+    public float rotationSpeed = 5f; // Geschwindigkeit der Rotation
+    private Quaternion[] initialRotations; // Speichert die Startrotationen der Segmente
+
+    void Start()
+    {
+        // Speichert die Anfangsrotationen der Segmente
+        initialRotations = new Quaternion[segments.Length];
+        for (int i = 0; i < segments.Length; i++)
+        {
+            initialRotations[i] = segments[i].rotation;
+        }
+    }
 
     void Update()
     {
-        // Wenn die Zahl 1 gedrückt wird, drehe alle Segmente außer 3 und 6 in X-Richtung
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.anyKeyDown)
         {
-            foreach (var segment in segments)
-            {
-                if (segment.name != "3" && segment.name != "6")  // Alle außer 3 und 6
-                {
-                    segment.Rotate(Vector3.right, 90); // Drehe in X-Richtung
-                }
-            }
-        }
+            ResetSegments(); // Setzt alle Segmente zurück, bevor eine neue Drehung beginnt
 
-        // Wenn die Zahl 2 gedrückt wird, drehe nur die Segmente 3 und 5 in Y-Richtung
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            foreach (var segment in segments)
-            {
-                if (segment.name == "3" || segment.name == "5")  // Nur 3 und 5
-                {
-                    segment.Rotate(Vector3.up, 90); // Drehe in Y-Richtung
-                }
-            }
+            if (Input.GetKeyDown(KeyCode.Alpha1)) RotateSegmentsSmoothly(new string[] { "1", "3", "4", "6", "7", "8" });
+            if (Input.GetKeyDown(KeyCode.Alpha2)) RotateSegmentsSmoothly(new string[] { "2", "6" }, Vector3.right);
+            if (Input.GetKeyDown(KeyCode.Alpha3)) RotateSegmentsSmoothly(new string[] { "2", "5" });
+            if (Input.GetKeyDown(KeyCode.Alpha4)) RotateSegmentsSmoothly(new string[] { "1", "5", "7" });
+            if (Input.GetKeyDown(KeyCode.Alpha5)) RotateSegmentsSmoothly(new string[] { "3", "5" }, Vector3.right);
+            if (Input.GetKeyDown(KeyCode.Alpha6)) RotateSegmentsSmoothly(new string[] { "3" });
+            if (Input.GetKeyDown(KeyCode.Alpha7)) RotateSegmentsSmoothly(new string[] { "2", "4", "5", "7", "8" });
+            if (Input.GetKeyDown(KeyCode.Alpha0)) RotateSegmentsSmoothly(new string[] { "4" });
+            if (Input.GetKeyDown(KeyCode.Alpha9)) RotateSegmentsSmoothly(new string[] { "5" });
         }
+    }
 
-        // Wenn die Zahl 3 gedrückt wird, drehe nur die Segmente 3 und 6
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+    void ResetSegments()
+    {
+        // Setzt alle Segmente auf ihre ursprüngliche Rotation zurück
+        for (int i = 0; i < segments.Length; i++)
         {
-            foreach (var segment in segments)
-            {
-                if (segment.name == "3" || segment.name == "6")  // Nur 3 und 6
-                {
-                    segment.Rotate(Vector3.right, 90); // Drehe in X-Richtung
-                }
-            }
+            segments[i].rotation = initialRotations[i];
         }
+    }
 
-        // Wenn die Zahl 4 gedrückt wird, drehe 1, 5 und 7
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            foreach (var segment in segments)
-            {
-                if (segment.name == "1" || segment.name == "5" || segment.name == "7")  // 1, 5 und 7
-                {
-                    segment.Rotate(Vector3.right, 90); // Drehe in X-Richtung
-                }
-            }
-        }
+    void RotateSegmentsSmoothly(string[] segmentNames, Vector3 axis = default)
+    {
+        if (axis == default) axis = Vector3.right; // Standardmäßig in X-Richtung drehen
 
-        // Wenn die Zahl 5 gedrückt wird, drehe nur 2 und 6
-        if (Input.GetKeyDown(KeyCode.Alpha5))
+        foreach (var segment in segments)
         {
-            foreach (var segment in segments)
+            if (System.Array.Exists(segmentNames, name => segment.name == name))
             {
-                if (segment.name == "2" || segment.name == "6")  // Nur 2 und 6
-                {
-                    segment.Rotate(Vector3.up, 90); // Drehe in Y-Richtung
-                }
-            }
-        }
-
-        // Wenn die Zahl 6 gedrückt wird, drehe nur das Segment 3
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            foreach (var segment in segments)
-            {
-                if (segment.name == "3")  // Nur 3
-                {
-                    segment.Rotate(Vector3.right, 90); // Drehe in X-Richtung
-                }
-            }
-        }
-
-        // Wenn die Zahl 7 gedrückt wird, drehe alle außer 1, 3 und 6
-        if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            foreach (var segment in segments)
-            {
-                if (segment.name != "1" && segment.name != "3" && segment.name != "6")  // Alle außer 1, 3 und 6
-                {
-                    segment.Rotate(Vector3.right, 90); // Drehe in X-Richtung
-                }
-            }
-        }
-
-        // Wenn die Zahl 0 gedrückt wird, drehe nur das Segment 4
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            foreach (var segment in segments)
-            {
-                if (segment.name == "4")  // Nur 4
-                {
-                    segment.Rotate(Vector3.right, 90); // Drehe in X-Richtung
-                }
-            }
-        }
-
-        // Wenn die Zahl 9 gedrückt wird, drehe nur das Segment 5
-        if (Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            foreach (var segment in segments)
-            {
-                if (segment.name == "5")  // Nur 5
-                {
-                    segment.Rotate(Vector3.right, 90); // Drehe in X-Richtung
-                }
+                StartCoroutine(RotateOverTime(segment, axis, 90f));
             }
         }
     }
+
+    IEnumerator RotateOverTime(Transform segment, Vector3 axis, float angle)
+    {
+        Quaternion startRotation = segment.rotation;
+        Quaternion endRotation = segment.rotation * Quaternion.AngleAxis(angle, axis);
+        float time = 0;
+
+        while (time < 1)
+        {
+            time += Time.deltaTime * rotationSpeed;
+            segment.rotation = Quaternion.Slerp(startRotation, endRotation, time);
+            yield return null;
+        }
+    }
 }
-
-
